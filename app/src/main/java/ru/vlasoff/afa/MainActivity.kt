13fdeb038cbefcacea94969f740d50cbcab4e7cb
vladimir <holdencolfild19@gmail.com>
2021-05-24 15:21:11 +0300

    Init project

---
+++ app/src/main/java/ru/vlasoff/afa/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.vlasoff.afa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
